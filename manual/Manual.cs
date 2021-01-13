a91909ba85102b07b61cd60b1a993e9d8d0b7271
Lunar Tiger <lunartiger@live.com>
2021-01-12 19:54:22 -0500

    :tada:

--- manual/Manual.cs
+++ manual/Manual.cs
@@ -10,6 +10,7 @@ using Sansar.Simulation;
 using Sansar.Script;
 using System;
 
+
 // To get full access to the Object API a script must extend from ObjectScript
 public class Manual : SceneObjectScript
 {
@@ -26,6 +27,8 @@ public class Manual : SceneObjectScript
 
     private void OnChat(ChatData data)
     {
+        SessionId ssid = data.SourceId;
+        AgentPrivate agent = ScenePrivate.FindAgent(ssid);
         // Parse the chat message into an array of strings and look for a "/man" command
         var cmds = data.Message.Split(new Char[] { ' ' });
         if (cmds[0] == "/man")
@@ -35,66 +38,65 @@ public class Manual : SceneObjectScript
                 // Lunar
                 if (cmds[1] == "lunar" || cmds[1] == "/lunar")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/lunar\nGives a link to Lunar's linktree.");
+                    agent.SendChat("/lunar\nGives a link to Lunar's linktree.");
                 }
                 // Gravity
                 else if (cmds[1] == "g" || cmds[1] == "/g")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/g\nA command to set the gravity.\nAccepts values from 0-49.\n************\nArguments:\ndefault - resets to default gravity\nplanets - lists the planets avaliable to set the gravity\n************\nExamples:\n/g 3.14\n/g default\n/g planets\n************\nSource Code:  https://lunartiger.github.io/sansar/gravity");
+                    agent.SendChat("/g\nA command to set the gravity.\nAccepts values from 0-49.\n************\nArguments:\ndefault - resets to default gravity\nplanets - lists the planets avaliable to set the gravity\n************\nExamples:\n/g 3.14\n/g default\n/g planets\n************\nSource Code:  https://lunartiger.github.io/sansar/gravity");
                 }
                 // tk
                 else if (cmds[1] == "tk" || cmds[1] == "/tk")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/tk\nPlays videos/displays webpages.\n************\nExamples:\n/tk https://lunartiger.gthub.io/tk\n/tk https://youtu.be/dQw4w9WgXcQ");
+                    agent.SendChat("/tk\nPlays videos/displays webpages.\n************\nExamples:\n/tk https://lunartiger.gthub.io/tk\n/tk https://youtu.be/dQw4w9WgXcQ");
                 }
                 // destinations/places
                 else if (cmds[1] == "destinations" || cmds[1] == "/destinations")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/destinations\nPrints out a list of the destinations within the world.");
+                    agent.SendChat("/destinations\nPrints out a list of the destinations within the world.");
                 }
                 else if (cmds[1] == "places" || cmds[1] == "/places")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/places\nPrints out a list of the destinations within the world.");
+                    agent.SendChat("/places\nPrints out a list of the destinations within the world.");
                 }
                 // in-world teleports
                 else if (cmds[1] == "ctrl" || cmds[1] == "/ctrl")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/ctrl\nTeleports you to the control room.\nIn world teleport.");
+                    agent.SendChat("/ctrl\nTeleports you to the control room.\nIn world teleport.");
                 }
                 else if (cmds[1] == "field" || cmds[1] == "/field")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/field\nTeleports you to a field where there is a large media screen.\nIn world teleport.");
+                    agent.SendChat("/field\nTeleports you to a field where there is a large media screen.\nIn world teleport.");
                 }
                 else if (cmds[1] == "house" || cmds[1] == "/house")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/house\nTeleports you to a tiny house.\nIn world teleport.");
+                    agent.SendChat("/house\nTeleports you to a tiny house.\nIn world teleport.");
                 }
                 else if (cmds[1] == "studio" || cmds[1] == "/studio")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/studio\nTeleports you to the studio area. This is the main spawn point of the world.\nIn world teleport.");
+                    agent.SendChat("/studio\nTeleports you to the studio area. This is the main spawn point of the world.\nIn world teleport.");
                 }
                 // void
                 else if (cmds[1] == "void" || cmds[1] == "/void")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/void\nTeleports you to the void.\n*This is another world.*");
+                    agent.SendChat("/void\nTeleports you to the void.\n*This is another world.*");
                 }
                 // man
                 else if (cmds[1] == "man" || cmds[1] == "/man")
                 {
-                    ScenePrivate.Chat.MessageAllUsers("/man\nThis is a helpful manual. Just use the command \"/man\" along with an avaliable command to find out more about it.\n************\nAvaliable Commands:\n" + AvaliableCommands + "\n************\nSource Code:  https://lunartiger.github.io/sansar/manual");
+                    agent.SendChat("/man\nThis is a helpful manual. Just use the command \"/man\" along with an avaliable command to find out more about it.\n************\nAvaliable Commands:\n" + AvaliableCommands + "\n************\nSource Code:  https://lunartiger.github.io/sansar/manual");
                 }
                 // if no matches found
                 else
                 {
                     //($"Unknown command. Avaliable Commands:  {AvaliableCommands}");
-                    //can't get that working so I'll continue messaging everyone I guess
-                    ScenePrivate.Chat.MessageAllUsers("Unknown command. Avaliable Commands:  " + AvaliableCommands);
+                    agent.SendChat("Unknown command. Avaliable Commands:  " + AvaliableCommands);
                 }
             }
             // If no additional parameter was specified, print out the avaliable commands.
             else
             {
-                ScenePrivate.Chat.MessageAllUsers("Avaliable Commands:  " + AvaliableCommands);
+                agent.SendChat("Avaliable Commands:  " + AvaliableCommands);
             }
         }
     }
