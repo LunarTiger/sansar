7cc859a029579f9147514441538bfc3878b8ee56
Lunar Tiger <lunartiger@live.com>
2021-01-14 01:48:25 -0500

    :tada:

--- manual/Manual.cs
+++ manual/Manual.cs
@@ -38,68 +38,68 @@ public class Manual : SceneObjectScript
                 // Lunar
                 if (cmds[1] == "lunar" || cmds[1] == "/lunar")
                 {
-                    agent.SendChat("/lunar\nDisplays \"Lunar: https://linktr.ee/lunar69\" which is a link to Lunar's linktree.");
+                    agent.SendChat("/lunar\n-Displays \"Lunar: https://linktr.ee/lunar69\" which is a link to Lunar's linktree.");
                 }
                 // Gravity
                 else if (cmds[1] == "g" || cmds[1] == "/g")
                 {
-                    agent.SendChat("/g\nA command to set the gravity.\nAccepts values from 0-49.\n************\nArguments:\n~help - displays help\n~default - resets to default gravity\n~reset - resets to default gravity\n~planets - lists the planets avaliable to set the gravity\n************\nExamples:\n/g 3.14\n/g default\n/g planets\n************\nSource Code:  https://lunartiger.github.io/sansar/gravity");
+                    agent.SendChat("/g\n-A command to set the gravity.\n-Accepts values from 0-49.\n************\nArguments:\nhelp - displays help\ndefault - resets to default gravity\nreset - resets to default gravity\nplanets - lists the planets avaliable to set the gravity\n************\nExamples:\n/g 3.14\n/g default\n/g planets\n************\nSource Code:  https://lunartiger.github.io/sansar/gravity");
                 }
                 // tk
                 else if (cmds[1] == "tk" || cmds[1] == "/tk")
                 {
-                    agent.SendChat("/tk\nPlays videos/displays webpages.\n************\nExamples:\n/tk https://lunartiger.gthub.io/tk\n/tk https://youtu.be/dQw4w9WgXcQ");
+                    agent.SendChat("/tk\n-Plays videos/displays webpages.\n************\nExamples:\n/tk https://lunartiger.gthub.io/tk\n/tk https://youtu.be/dQw4w9WgXcQ");
                 }
                 // destinations/places
                 else if (cmds[1] == "destinations" || cmds[1] == "/destinations")
                 {
-                    agent.SendChat("/destinations\nPrints out a list of the destinations within the world.\n~Same as /places");
+                    agent.SendChat("/destinations\n-Prints out a list of the destinations within the world.\n~Same as /places");
                 }
                 else if (cmds[1] == "places" || cmds[1] == "/places")
                 {
-                    agent.SendChat("/places\nPrints out a list of the destinations within the world.\n~Same as /destinations");
+                    agent.SendChat("/places\n-Prints out a list of the destinations within the world.\n~Same as /destinations");
                 }
                 // in-world teleports
                 else if (cmds[1] == "ctrl" || cmds[1] == "/ctrl")
                 {
-                    agent.SendChat("/ctrl\nTeleports you to the control room.\nIn world teleport.");
+                    agent.SendChat("/ctrl\n-Teleports you to the control room.\n~In world teleport.");
                 }
                 else if (cmds[1] == "field" || cmds[1] == "/field")
                 {
-                    agent.SendChat("/field\nTeleports you to a field where there is a large media screen.\nIn world teleport.");
+                    agent.SendChat("/field\n-Teleports you to a field where there is a large media screen.\n~In world teleport.");
                 }
                 else if (cmds[1] == "house" || cmds[1] == "/house")
                 {
-                    agent.SendChat("/house\nTeleports you to a tiny house.\nIn world teleport.");
+                    agent.SendChat("/house\n-Teleports you to a tiny house.\n~In world teleport.");
                 }
                 else if (cmds[1] == "studio" || cmds[1] == "/studio")
                 {
-                    agent.SendChat("/studio\nTeleports you to the studio area. This is the main spawn point of the world.\nIn world teleport.");
+                    agent.SendChat("/studio\n-Teleports you to the studio area.\n-This is the main spawn point of the world.\n~In world teleport.");
                 }
                 // void
                 else if (cmds[1] == "void" || cmds[1] == "/void")
                 {
-                    agent.SendChat("/void\nTeleports you to the void.\n*This is another world.*");
+                    agent.SendChat("/void\n-Teleports you to the void.\n~*This is another world.*");
                 }
                 // man
                 else if (cmds[1] == "man" || cmds[1] == "/man")
                 {
-                    agent.SendChat("/man\nThis is a helpful manual. Just use the command \"/man\" along with an avaliable command to find out more about it.\n************\nAvaliable Commands:\n" + AvaliableCommands + "\n************\nSource Code:  https://lunartiger.github.io/sansar/manual");
+                    agent.SendChat("/man\n-This is a helpful manual.\n-Just use the command \"/man\" along with an avaliable command to find out more about it.\n************\nAvaliable Commands:  " + AvaliableCommands + "\n************\nSource Code:  https://lunartiger.github.io/sansar/manual");
                 }
                 //afk/brb
                 else if (cmds[1] == "afk" || cmds[1] == "/afk")
                 {
-                    agent.SendChat("afk\nTyping \"afk\" in chat will make you do an emote, making it obvious you're not available.\nWorks with or wothout \"/\"\n~Same as brb");
+                    agent.SendChat("afk\n-Typing \"afk\" in chat will make you do an emote, making it obvious you're not available.\n-Works with or without \"/\"\n~Same as brb");
                 }
                 else if (cmds[1] == "brb" || cmds[1] == "/brb")
                 {
-                    agent.SendChat("brb\nTyping \"brb\" in chat will make you do an emote, making it obvious you're not available.\nWorks with or wothout \"/\"\n~Same as afk");
+                    agent.SendChat("brb\n-Typing \"brb\" in chat will make you do an emote, making it obvious you're not available.\n-Works with or without \"/\"\n~Same as afk");
                 }
                 // if no matches found
                 else
                 {
                     //($"Unknown command. Avaliable Commands:  {AvaliableCommands}");
-                    agent.SendChat("Unknown command ¯\\_(ツ)_/¯\n************\nAvaliable Commands:\n" + AvaliableCommands);
+                    agent.SendChat("Unknown command ¯\\_(ツ)_/¯\n************\nAvaliable Commands:  " + AvaliableCommands);
                 }
             }
             // If no additional parameter was specified, print out the avaliable commands.
